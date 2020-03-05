2d64d92bff41ab70c0d10ec3d49cddceafdd457e
Martin D. Jaere <martin@jaere.no>
2020-03-05 15:05:31 +0000

    Simplify by removing env option for dev server

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
 from app import app
 
 if __name__ == "__main__":
-    if os.getenv("FLASK_ENV", "development") != "production":
-        app.debug = True
+    app.debug = True
     app.run(host='127.0.0.1', port=8080)
